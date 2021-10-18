ef1d1e0758eea19b270c837ff0d4d761ea64f3fa
Joan A <91660058+zatafa@users.noreply.github.com>
2021-10-18 12:47:29 +0200

    Update mobilenet.py

--- keras_segmentation/models/mobilenet.py
+++ keras_segmentation/models/mobilenet.py
@@ -107,7 +107,7 @@ def get_mobilenet_encoder(input_height=224, input_width=224,
         model_name = 'mobilenet_%s_%d_tf_no_top.h5' % ('1_0', 224)
 
         weight_path = BASE_WEIGHT_PATH + model_name
-        weights_path = keras.utils.get_file(model_name, weight_path)
+        weights_path = tf.keras.utils.get_file(model_name, weight_path)
 
         Model(img_input, x).load_weights(weights_path, by_name=True, skip_mismatch=True)
 
