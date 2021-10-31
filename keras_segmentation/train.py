0dd48968df5c15eb803d435e6e99fb270b67373b
Joan A <91660058+zatafa@users.noreply.github.com>
2021-10-31 16:48:15 +0100

    Update train.py

--- keras_segmentation/train.py
+++ keras_segmentation/train.py
@@ -56,7 +56,16 @@ class CheckpointsCallback(Callback):
             self.model.save_weights(self.checkpoints_path + "." + str(epoch))
             print("saved ", self.checkpoints_path + "." + str(epoch))
 
+# Introduce Dice Coeff as metrics
+def dice_coeff(y_true, y_pred):
+    smooth = 1.
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    score = (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+    return score
 
+# Set the parameters / hyperparameters for training
 def train(model,
           train_images,
           train_annotations,
