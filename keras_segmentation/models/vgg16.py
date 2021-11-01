28d95eeebecf12490f96be4607f1a69096b6ec6b
Joan A <91660058+zatafa@users.noreply.github.com>
2021-11-01 10:26:28 +0100

    Update vgg16.py

--- keras_segmentation/models/vgg16.py
+++ keras_segmentation/models/vgg16.py
@@ -77,7 +77,7 @@ def get_vgg_encoder(input_height=224,  input_width=224, pretrained='imagenet', c
     
     # Add tensorflow framework on "get_file"
     if pretrained == 'imagenet':
-        VGG_Weights_path = tf.keras.utils.get_file(
+        VGG_Weights_path = tensorflow.keras.utils.get_file(
             pretrained_url.split("/")[-1], pretrained_url)
         Model(img_input, x).load_weights(VGG_Weights_path, by_name=True, skip_mismatch=True)
 
